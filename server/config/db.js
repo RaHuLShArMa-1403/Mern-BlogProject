5cbe6ffaea9aec7c5739c9830c2c0bf78165ec8b
RaHuLShArMa-1403 <rahul1123.be21@chitkara.edu.in>
2024-09-14 00:24:46 +0530

    updated db2

--- server/config/db.js
+++ server/config/db.js
@@ -3,10 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    const conn = await mongoose.connect(`${process.env.MONGODB_URI}?tls=true`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      tls: true, // or ssl: true depending on your MongoDB driver version
     });
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
