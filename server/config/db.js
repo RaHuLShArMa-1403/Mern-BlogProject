2749e19f88b4dbe82d282cb76b8fb17b2820300c
RaHuLShArMa-1403 <rahul1123.be21@chitkara.edu.in>
2024-09-14 00:14:41 +0530

    db updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,17 @@
-const mongoose = require('mongoose');
-const connectDB = async () =>{
+const mongoose = require("mongoose");
 
-    try{
-        mongoose.set('strictQuery', false);
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`Database Connected: ${conn.connection.host}`)
-    }
-    catch(error){
-        console.log(error);
-    }
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      tls: true, // or ssl: true depending on your MongoDB driver version
+    });
+    console.log(`Database Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("Database connection error:", error);
+  }
+};
 
-
-}
 module.exports = connectDB;
