0eb2bd020746a013f432235397eee12a286fafe0
RaHuLShArMa-1403 <rahul1123.be21@chitkara.edu.in>
2024-09-18 18:36:51 +0530

    correcting the path in app.js

--- app.js
+++ app.js
@@ -23,7 +23,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(methodOverride("_method"));
 
-app.use(favicon(__dirname + "/public/img/blog-logo.png"));
+const path = require("path");
+//const favicon = require('serve-favicon');
+
+app.use(favicon(path.join(__dirname, "public", "img", "favicon.png")));
 
 app.use(
   session({
